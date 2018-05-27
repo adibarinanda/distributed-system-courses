4cfe94720db9d26a5d3eeb8ddce670b64b0560ea
cakthe <alfatih_af@yahoo.co.id>
2018-05-27 20:44:08 +0700

    testing server-client

--- Final Project/client.py
+++ Final Project/client.py
@@ -1,8 +1,9 @@
 import Pyro4
 
 def main():
-	with Pyro4.core.Proxy("PYRO:dispatcher@192.168.56.101:") as d:
-		return "a"
+	with Pyro4.core.Proxy("PYRO:dispatcher@192.168.56.101:40858") as d:
+		# print("Test")
+		d.init()
 
 if __name__ == '__main__':
 	main()
--- Final Project/server.py
+++ Final Project/server.py
@@ -1,19 +1,18 @@
 import Pyro4
 
-
+@Pyro4.expose
 class FileTransfer(object):
     """docstring for FileTransfer"""
     """ def __init__(self, arg):
         super(FileTransfer, self).__init__()
         self.arg = arg """
-    def __init__(self):
-        return "Success"
+    def init(self):
+        print("Success")
         
 
 if __name__ == "__main__":
-    print("Spinning up 5 wordcounters, and 1 dispatcher.")
     custom_daemon = Pyro4.Daemon(host="192.168.56.101")  # some additional custom configuration
-    Pyro4.config.SERVERTYPE = "thread"
+    # Pyro4.config.SERVERTYPE = "thread"
     Pyro4.Daemon.serveSimple(
         {
             FileTransfer: "dispatcher"
